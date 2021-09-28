a44243e60d00b32ad47df2256c3674ad130438b9
zhipei.sun <zhipei.sun@hiscene>
2021-09-28 10:15:53 +0800

    switch to gin for web framework

--- demo/geoindex/geoindex.go
+++ demo/geoindex/geoindex.go
@@ -1,24 +1,44 @@
-package hello
+package main
 
 import (
-	"appengine"
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	index "github.com/hailocab/go-geoindex"
 	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
+
+	"log"
+
+	"github.com/gin-gonic/gin"
+	index "github.com/hailocab/go-geoindex"
 )
 
 func init() {
-	http.HandleFunc("/points", points)
-	http.HandleFunc("/knearest", knearest)
+	// http.HandleFunc("/points", points)
+	// http.HandleFunc("/knearest", knearest)
+}
+func main() {
+	if false { //test
+		nearest := getIndex().KNearest(index.NewGeoPoint("query", 18.230292850934863, -63.16093794856357), int(1000), index.Km(5), func(_ index.Point) bool { return true })
+		fmt.Println(nearest)
+		return
+	}
+	e := gin.Default()
+	e.Static("/static", "../static/")
+	groupName := ""
+	group := e.Group(groupName)
+	{
+		group.GET("/knearest", knearest4Gin)
+		group.GET("/points", points4Gin)
+		e.Run(":8090")
+	}
 }
 
 var geoindex *index.ClusteringIndex
 
+// 获取随机的正负
 func sign() float64 {
 	if rand.Float64() > 0.5 {
 		return 1
@@ -26,11 +46,11 @@ func sign() float64 {
 	return -1
 }
 
-func getIndex(context appengine.Context) *index.ClusteringIndex {
+func getIndex() *index.ClusteringIndex {
 	if geoindex == nil {
 		geoindex = index.NewClusteringIndex()
 
-		capitals := worldCapitals(context)
+		capitals := worldCapitals()
 		id := 1
 
 		for _, capital := range capitals {
@@ -49,11 +69,11 @@ func getIndex(context appengine.Context) *index.ClusteringIndex {
 	return geoindex
 }
 
-func worldCapitals(context appengine.Context) []index.Point {
-	file, err := os.OpenFile("static/capitals.csv", os.O_RDONLY, 0)
+func worldCapitals() []index.Point {
+	file, err := os.OpenFile("../static/capitals.csv", os.O_RDONLY, 0)
 
 	if err != nil {
-		context.Errorf("%v", err)
+		log.Printf("%v", err)
 		return make([]index.Point, 0)
 	}
 
@@ -77,6 +97,38 @@ func worldCapitals(context appengine.Context) []index.Point {
 	return capitals
 }
 
+type Point struct {
+	Lat float64 `form:"lat"`
+	Lon float64 `form:"lon"`
+	K   int64   `form:"k"`
+}
+
+func knearest4Gin(c *gin.Context) {
+	p := Point{}
+	c.ShouldBind(&p)
+
+	nearest := getIndex().KNearest(index.NewGeoPoint("query", p.Lat, p.Lon), int(p.K), index.Km(5), func(_ index.Point) bool { return true })
+	// data, _ := json.Marshal(nearest)
+	c.JSON(http.StatusOK, nearest)
+}
+
+type Rectangle struct {
+	TopLeftLat     float64 `form:"topLeftLat"`
+	TopLeftLon     float64 `form:"topLeftLon"`
+	BottomRightLat float64 `form:"bottomRightLat"`
+	BottomRightLon float64 `form:"bottomRightLon"`
+}
+
+func points4Gin(c *gin.Context) {
+	rec := Rectangle{}
+	c.BindQuery(&rec)
+
+	visiblePoints := getIndex().Range(index.NewGeoPoint("topLeft", rec.TopLeftLat, rec.TopLeftLon),
+		index.NewGeoPoint("bottomRight", rec.BottomRightLat, rec.BottomRightLon))
+
+	c.JSON(http.StatusOK, visiblePoints)
+}
+
 func points(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	topLeftLat, _ := strconv.ParseFloat(r.Form["topLeftLat"][0], 64)
@@ -84,8 +136,7 @@ func points(w http.ResponseWriter, r *http.Request) {
 	bottomRightLat, _ := strconv.ParseFloat(r.Form["bottomRightLat"][0], 64)
 	bottomRightLon, _ := strconv.ParseFloat(r.Form["bottomRightLon"][0], 64)
 
-	c := appengine.NewContext(r)
-	visiblePoints := getIndex(c).Range(index.NewGeoPoint("topLeft", topLeftLat, topLeftLon), index.NewGeoPoint("bottomRight", bottomRightLat, bottomRightLon))
+	visiblePoints := getIndex().Range(index.NewGeoPoint("topLeft", topLeftLat, topLeftLon), index.NewGeoPoint("bottomRight", bottomRightLat, bottomRightLon))
 
 	data, _ := json.Marshal(visiblePoints)
 	fmt.Fprintln(w, string(data))
@@ -97,8 +148,7 @@ func knearest(w http.ResponseWriter, r *http.Request) {
 	lon, _ := strconv.ParseFloat(r.Form["lon"][0], 64)
 	k, _ := strconv.ParseInt(r.Form["k"][0], 10, 32)
 
-	c := appengine.NewContext(r)
-	nearest := getIndex(c).KNearest(index.NewGeoPoint("query", lat, lon), int(k), index.Km(5), func(_ index.Point) bool { return true })
+	nearest := getIndex().KNearest(index.NewGeoPoint("query", lat, lon), int(k), index.Km(5), func(_ index.Point) bool { return true })
 	data, _ := json.Marshal(nearest)
 	fmt.Fprintln(w, string(data))
 }
---
+++ demo/geoindex/go.mod
@@ -0,0 +1,9 @@
+module geoindex
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.4 // indirect
+	github.com/hailocab/go-geoindex v0.0.0-20160127134810-64631bfe9711 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
