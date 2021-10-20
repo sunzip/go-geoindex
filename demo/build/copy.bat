17d1b60bd73a33e49bc08b6c1c614cd62c5a1774
zhipei.sun <zhipei.sun@hiscene.com>
2021-10-20 19:03:21 +0800

    移除zip文件；优化one-click

--- demo/build/copy.bat
+++ demo/build/copy.bat
@@ -1,8 +1,16 @@
+:: 解决bat utf-8编码，显示乱码问题
+CHCP 65001 
+:: 解决将新的内容压缩到旧zip文件里
+del map.zip 
+
 mkdir ..\map\static
 mkdir ..\map\static-gaode
+mkdir ..\map\static-gaode\效果
 :: copy /y ..\server  +  ..\index.html ..\map
 copy /y ..\static ..\map\static
 copy /y ..\static-gaode ..\map\static-gaode
+:: copy /y ..\static-gaode\效果 ..\map\static-gaode\效果
+XCOPY ..\static-gaode\效果\  ..\map\static-gaode\效果\  /E /Y
 :: 可执行文件不需要保留到原始目录
 move /y ..\server ..\map 
 move /y ..\server.exe ..\map
